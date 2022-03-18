81ee6a480e2e07af562b77d2c22abaefc6c7bedf
Nitorogi <101595067+Nitorogi@users.noreply.github.com>
2022-03-18 14:10:53 +0200

    добавление персонажей второй части (1/2)

---
+++ gamemode/framework/teams/sh_fuyuhiko.lua
@@ -0,0 +1,35 @@
+--[[
+        © Asterion Project 2021.
+        This script was created from the developers of the AsterionTeam.
+        You can get more information from one of the links below:
+            Site - https://asterionproject.ru
+            Discord - https://discord.gg/Cz3EQJ7WrF
+        
+        developer(s):
+            Selenter - https://steamcommunity.com/id/selenter
+        ——— Chop your own wood and it will warm you twice.
+]]--
+
+local TEAM = {emodjiList = {}}
+
+TEAM.name = "Фуюхико Кузурю"
+TEAM.description = "Абсолютный Якудза"
+TEAM.category = "GOODBYE DESPAIR"
+TEAM.model = "models/player/dewobedil/danganronpa/fuyuhiko/default_p.mdl"
+TEAM.path = "danganronpa/characters/fuyuhiko"
+
+TEAM.color = Color(240, 201, 73)
+TEAM.evidenceVisibility = 0.6
+TEAM.staminaSpeed = 1
+TEAM.walkSpeed = 1
+TEAM.runSpeed = 1
+TEAM.hungerSpeed = 33
+TEAM.thirstSpeed = 33
+TEAM.sleepSpeed = 33
+TEAM.max = 1
+
+for i = 1, 16 do
+	TEAM.emodjiList[#TEAM.emodjiList + 1] = TEAM.path .. "/emoji/" .. i .. ".png"
+end
+
+TEAM_FUYUHIKO = Arbitrage.teams.Create(TEAM)
---
+++ gamemode/framework/teams/sh_sonia.lua
@@ -0,0 +1,35 @@
+--[[
+        © Asterion Project 2021.
+        This script was created from the developers of the AsterionTeam.
+        You can get more information from one of the links below:
+            Site - https://asterionproject.ru
+            Discord - https://discord.gg/Cz3EQJ7WrF
+        
+        developer(s):
+            Selenter - https://steamcommunity.com/id/selenter
+        ——— Chop your own wood and it will warm you twice.
+]]--
+
+local TEAM = {emodjiList = {}}
+
+TEAM.name = "Сония Невермайнд"
+TEAM.description = "Абсолютная Принцесса"
+TEAM.category = "GOODBYE DESPAIR"
+TEAM.model = "models/player/dewobedil/danganronpa/sonia_nevermind/default_p.mdl"
+TEAM.path = "danganronpa/characters/sonia"
+
+TEAM.color = Color(240, 201, 73)
+TEAM.evidenceVisibility = 0.6
+TEAM.staminaSpeed = 1
+TEAM.walkSpeed = 1
+TEAM.runSpeed = 1
+TEAM.hungerSpeed = 33
+TEAM.thirstSpeed = 33
+TEAM.sleepSpeed = 33
+TEAM.max = 1
+
+for i = 1, 16 do
+	TEAM.emodjiList[#TEAM.emodjiList + 1] = TEAM.path .. "/emoji/" .. i .. ".png"
+end
+
+TEAM_SONIA = Arbitrage.teams.Create(TEAM)

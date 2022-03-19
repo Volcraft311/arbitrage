91849f2df4d23007ed9a3231d0f00e0e4c502572
Selenter <samkolp@mail.ru>
2022-03-19 22:12:24 +0300

    Оптимизация AdminESP

--- gamemode/framework/plugins/adminesp/cl_plugin.lua
+++ gamemode/framework/plugins/adminesp/cl_plugin.lua
@@ -21,11 +21,11 @@ surface.CreateFont( "AdminESPFont", {
 } )
 
 local function createText(data, x, y, col, y2)
-	draw.SimpleText(data, "AdminESPFont", x, y + y2 - 1, Color(0, 0, 0), TEXT_ALIGN_LEFT)
-	draw.SimpleText(data, "AdminESPFont", x, y + y2 + 1, Color(0, 0, 0), TEXT_ALIGN_LEFT)
-	draw.SimpleText(data, "AdminESPFont", x - 1, y + y2, Color(0, 0, 0), TEXT_ALIGN_LEFT)
-	draw.SimpleText(data, "AdminESPFont", x + 1, y + y2, Color(0, 0, 0), TEXT_ALIGN_LEFT)
-	draw.SimpleText(data, "AdminESPFont", x, y + y2, col, TEXT_ALIGN_LEFT)
+	-- draw.DrawText(data, "AdminESPFont", x, y + y2 - 1, Color(0, 0, 0), TEXT_ALIGN_LEFT)
+	-- draw.DrawText(data, "AdminESPFont", x, y + y2 + 1, Color(0, 0, 0), TEXT_ALIGN_LEFT)
+	-- draw.DrawText(data, "AdminESPFont", x - 1, y + y2, Color(0, 0, 0), TEXT_ALIGN_LEFT)
+	-- draw.DrawText(data, "AdminESPFont", x + 1, y + y2, Color(0, 0, 0), TEXT_ALIGN_LEFT)
+	draw.DrawText(data, "AdminESPFont", x, y + y2, col, TEXT_ALIGN_LEFT)
 end
 
 function PLUGIN:HUDPaint()
@@ -43,25 +43,25 @@ function PLUGIN:HUDPaint()
 		if !p and !self.entslist[v:GetClass()] then continue end
 
 		if !p or (p and v != LocalPlayer() and v:oldAlive()) then
-			local _y = 0
 			local info = v:ESPInfo()
+
+			local pos = v:GetPos()
+			local head = Vector(pos.x, pos.y, !p and pos.z or pos.z + 60)
+			local headPos = head:ToScreen()
+			local distance = LocalPlayer():GetPos():Distance(v:GetPos())
+			local x, y = headPos.x, headPos.y
+			local f = math.abs(350 / distance)
+			local size = 52 * f
+			local col = (p and team.GetColor(v:Team()) or self.entslist[v:GetClass()]) or color_white
+			local text = ""
 
 			for k2, v2 in pairs(info) do
 				if v2[1] and self:DistanceFits(LocalPlayer():GetPos(), v:GetPos(), v2[2]) then
-					local pos = v:GetPos()
-					local head = Vector(pos.x, pos.y, !p and pos.z or pos.z + 60)
-					local headPos = head:ToScreen()
-					local distance = LocalPlayer():GetPos():Distance(v:GetPos())
-					local x, y = headPos.x, headPos.y
-					local f = math.abs(350 / distance)
-					local size = 52 * f
-					local col = (p and team.GetColor(v:Team()) or self.entslist[v:GetClass()]) or color_white
-
-					createText(tostring(v2[1]), (x - size / 2) + size, y - size / 2, col, _y)
-
-					_y = _y + 15
+					text = text .. v2[1] .. "\n"
 				end
 			end
+
+			createText(text, (x - size / 2) + size, y - size / 2, col, 0)
 		end
 	end
 end

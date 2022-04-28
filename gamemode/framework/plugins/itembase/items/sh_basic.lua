b823bf18e3ddb7e362a0efd12f8f16d05f6f2ce1
Selenter <samkolp@mail.ru>
2022-04-28 23:51:06 +0300

    Добавление стандартной базы предметов (мусор)

---
+++ gamemode/framework/plugins/itembase/items/sh_basic.lua
@@ -0,0 +1,249 @@
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Чайник"
+	ITEM.description = "Предназначен для кипячения воды"
+	ITEM.model = "models/props_interiors/pot01a.mdl"
+
+	ItemBase:RegisterItem("teapot", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Пластиковый ящик"
+	ITEM.description = "Предназначенная для хранения и транспортировки различных видов продуктов."
+	ITEM.model = "models/props_junk/PlasticCrate01a.mdl"
+
+	ItemBase:RegisterItem("plastic_box", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Фигурка рыбы"
+	ITEM.description = "Эту вещь можно подарить кому-то."
+	ITEM.model = "models/aschool25/mudfish.mdl"
+
+	ItemBase:RegisterItem("fish_toy", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Мусорное ведро"
+	ITEM.description = "Ёмкость для сбора мусора."
+	ITEM.model = "models/aschool25/musornoevedro.mdl"
+
+	ItemBase:RegisterItem("trash_can", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Компакт-диск"
+	ITEM.description = "Оптический носитель информации."
+	ITEM.model = "models/props_vtmb/cdcase.mdl"
+
+	ItemBase:RegisterItem("CD", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Зубная щётка"
+	ITEM.description = "Инструмент для удаления мягких отложений с поверхности зубов и дёсен."
+	ITEM.model = "models/props_vtmb/toothbrush.mdl"
+
+	ItemBase:RegisterItem("toothbrush", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Дверной замок"
+	ITEM.description = "Применяется для запирания дверей."
+	ITEM.model = "models/props_vtmb/padlock1.mdl"
+
+	ItemBase:RegisterItem("padlock", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Степлер"
+	ITEM.description = "Устройство для скрепления листов бумаги металлическими скобами."
+	ITEM.model = "models/props_vtmb/stapler.mdl"
+
+	ItemBase:RegisterItem("stapler", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Ручка"
+	ITEM.description = "Письменная принадлежность."
+	ITEM.model = "models/props_vtmb/pen.mdl"
+
+	ItemBase:RegisterItem("pen", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Пипидастр"
+	ITEM.description = "Используется для уборки."
+	ITEM.model = "models/props_vtmb/feather_duster.mdl"
+
+	ItemBase:RegisterItem("pipidaster", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Жидкое мыло"
+	ITEM.description = "Косметический продукт, предназначенный для очищения и ухода за кожей."
+	ITEM.model = "models/props_interiors/soap_dispenser.mdl"
+
+	ItemBase:RegisterItem("soap_dispenser", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Глобус"
+	ITEM.description = "Трёхмерная модель Земли."
+	ITEM.model = "models/props_combine/breenglobe.mdl"
+
+	ItemBase:RegisterItem("breenglobe", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Кукла"
+	ITEM.description = "Предмет используется в качестве игрушки для маленьких детей."
+	ITEM.model = "models/props_c17/doll01.mdl"
+
+	ItemBase:RegisterItem("doll", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Банка краски"
+	ITEM.description = "В данной таре хранится краска."
+	ITEM.model = "models/props_junk/plasticbucket001a.mdl"
+
+	ItemBase:RegisterItem("can_of_paint", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Канистра с бензином"
+	ITEM.description = "Ёмкость, в которой хранится бензин."
+	ITEM.model = "models/props_junk/metalgascan.mdl"
+
+	ItemBase:RegisterItem("metalgascan", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Ведро"
+	ITEM.description = "Сосуд для хранения жидких и сыпучих материалов и транспортировки их на небольшие расстояния."
+	ITEM.model = "models/props_junk/MetalBucket01a.mdl"
+
+	ItemBase:RegisterItem("metalbucket", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Батарея"
+	ITEM.description = "Соединение нескольких однотипных приборов, устройств в единую систему."
+	ITEM.model = "models/props_wasteland/prison_heater001a.mdl"
+
+	ItemBase:RegisterItem("prison_heater", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Кактус"
+	ITEM.description = "Этот предмет можно подарить кому-нибудь."
+	ITEM.model = "models/props_lab/cactus.mdl"
+
+	ItemBase:RegisterItem("cactus", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Сковорода"
+	ITEM.description = "Посуда для жарки пищи."
+	ITEM.model = "models/props_c17/metalPot002a.mdl"
+
+	ItemBase:RegisterItem("frying_pan", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Часы"
+	ITEM.description = "Прибор для определения текущего времени суток."
+	ITEM.model = "models/props_c17/clock01.mdl"
+
+	ItemBase:RegisterItem("clock", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Кружка"
+	ITEM.description = "Полое изделие различной формы, преимущественно с ручкой, предназначенное для питья."
+	ITEM.model = "models/props_junk/garbage_coffeemug001a.mdl"
+
+	ItemBase:RegisterItem("cup", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Белизна"
+	ITEM.description = "Бытовое отбеливающее средство на основе гипохлорита натрия."
+	ITEM.model = "models/props_junk/garbage_plasticbottle002a.mdl"
+
+	ItemBase:RegisterItem("whiteness", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Бутылка пива"
+	ITEM.description = "Слабоалкогольный напиток."
+	ITEM.model = "models/props_junk/garbage_glassbottle001a.mdl"
+
+	ItemBase:RegisterItem("bear", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Горшок"
+	ITEM.description = "Собирательное название разнообразных, обычно низких устойчивых, керамических сосудов с широким горлом."
+	ITEM.model = "models/props_junk/terracotta01.mdl"
+
+	ItemBase:RegisterItem("terracotta", ITEM)
+end
+
+do
+	local ITEM = ItemBase.GetBase()
+
+	ITEM.name = "Радио"
+	ITEM.description = "Способ передачи сообщений на расстояние посредством радиоволн."
+	ITEM.model = "models/props_lab/citizenradio.mdl"
+
+	ItemBase:RegisterItem("radio", ITEM)
+end

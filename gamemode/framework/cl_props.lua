87c22a9da4c7e150563ae34d7d8a3d940d44da65
Selenter <samkolp@mail.ru>
2022-05-06 23:48:22 +0300

    add copyright

--- gamemode/framework/cl_props.lua
+++ gamemode/framework/cl_props.lua
@@ -1,3 +1,17 @@
+--[[
+        © AsterionStaff 2022.
+        This script was created from the developers of the Asterion Staff.
+        You can get more information from one of the links below:
+            Site - https://asterionproject.ru (not work)
+            Discord - https://discord.gg/Np5evb5ZsR
+        
+        developer(s):
+            Selenter - https://steamcommunity.com/id/selenter
+
+        ——— Chop your own wood and it will warm you twice.
+]]--
+
+
 local data = {
     ["Developer Style Props"] = {
         "models/props/dev_barrel32x48.mdl",
@@ -479,13 +493,13 @@ function Arbitrage:PopulatePropMenu()
     local contents = {}
 
     for k, v in pairs(data) do
-        table.insert( contents, {
+        table.insert(contents, {
             type = "header",
             text = k
         })
 
         for k2, v2 in ipairs(v) do
-            table.insert( contents, {
+            table.insert(contents, {
                 type = "model",
                 model = v2
             })

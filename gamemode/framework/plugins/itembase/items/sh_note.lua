9b2ba48e7100a37454f2868e5545920ffe0f3f9e
Nitorogi <101595067+Nitorogi@users.noreply.github.com>
2022-05-22 12:32:46 +0300

    Update sh_note.lua

--- gamemode/framework/plugins/itembase/items/sh_note.lua
+++ gamemode/framework/plugins/itembase/items/sh_note.lua
@@ -18,7 +18,7 @@ do
     ITEM.name = "Блокнот"
     ITEM.description = "Самый обычный блокнот, скорее всего содержит в себе какие-то записи."
     ITEM.model = "models/props_vtmb/dayplanner_closed.mdl"
-    ITEM.icon = "danganronpa/inventory/items/papernotebook.png"
+    ITEM.icon = "danganronpa/inventory/items/notebook_paper.png"
 
     ItemBase:RegisterItem("notepad", ITEM)
 end

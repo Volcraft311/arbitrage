7a686bdd75cf39f18f6486f7fe152f1f2e5737ab
Nitorogi <101595067+Nitorogi@users.noreply.github.com>
2022-05-29 12:10:21 +0300

    Update sh_kyoko.lua

--- gamemode/framework/teams/sh_kyoko.lua
+++ gamemode/framework/teams/sh_kyoko.lua
@@ -16,7 +16,7 @@ local TEAM = {emodjiList = {}}
 TEAM.name = "Кёко Киригири"
 TEAM.description = "Абсолютный Детектив"
 TEAM.category = "TRIGGER HAPPY HAVOC"
-TEAM.model = "models/kyoko_kirigiri_yoru/danganronpa/rstar/kyoko_kirigiri_yoru"
+TEAM.model = "models/kyoko_kirigiri_yoru/danganronpa/rstar/kyoko_kirigiri_yoru/kyoko_kirigiri_yoru.mdl"
 TEAM.path = "danganronpa/characters/kyoko"
 
 TEAM.color = Color(240, 201, 73)

350ae0edcdae0b82187666527df818de945463d0
Nitorogi <101595067+Nitorogi@users.noreply.github.com>
2022-06-04 14:34:33 +0300

    Новая модель Саяки

--- gamemode/framework/teams/sh_sayaka.lua
+++ gamemode/framework/teams/sh_sayaka.lua
@@ -16,7 +16,7 @@ local TEAM = {emodjiList = {}}
 TEAM.name = "Саяка Майзоно"
 TEAM.description = "Абсолютная Поп-Звезда"
 TEAM.category = "TRIGGER HAPPY HAVOC"
-TEAM.model = "models/player/dewobedil/sayaka_maizono/default_p.mdl"
+TEAM.model = "models/sayaka_yoru/danganronpa/rstar/sayaka_yoru/sayaka_yoru.mdl"
 TEAM.path = "danganronpa/characters/sayaka"
 
 TEAM.color = Color(240, 201, 73)

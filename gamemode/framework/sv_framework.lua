eb0a8b7a8968e49fc7e98cf8e7c42410a70286e8
Selenter <samkolp@mail.ru>
2022-06-05 18:20:50 +0300

    Консольная команда `arb_returnnormalflashlight`

--- gamemode/framework/sv_framework.lua
+++ gamemode/framework/sv_framework.lua
@@ -654,6 +654,23 @@ concommand.Add("arb_join_notcharacter", function(client, command, arguments)
     end
 end)
 
+concommand.Add("arb_returnnormalflashlight", function(client, command, arguments)
+    if !client:IsAdmin() then return end
+
+    hook.Remove("PlayerSwitchFlashlight", "TPF_SwitchFlashlightHook")
+    hook.Remove("PlayerInitialSpawn", "TPF_InitServerDefaults")
+    hook.Remove("PlayerPostThink", "TPF_Update")
+
+    hook.Add("PlayerSwitchFlashlight", "arb.AllowFlashlight", function()
+        return true
+    end)
+
+    for k, v in ipairs(player.GetAll()) do
+        v:AllowFlashlight(true)
+        v:Flashlight(true)
+    end
+end)
+
 timer.Create("arb.CurTime", 1, 0, function()
     Arbitrage.CurTime = Arbitrage.CurTime or 0
     Arbitrage.CurTime = Arbitrage.CurTime + 17

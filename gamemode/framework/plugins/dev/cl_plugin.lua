da9931e28c6d9e6b9cf1b1328eccc8c5c9c2432f
Selenter <samkolp@mail.ru>
2022-08-30 16:45:56 +0300

    Обновление вывода информации о режиме в верхнем углу

--- gamemode/framework/plugins/dev/cl_plugin.lua
+++ gamemode/framework/plugins/dev/cl_plugin.lua
@@ -18,25 +18,24 @@ local ScrW = ScrW
 local Arbitrage = Arbitrage
 local Color = Color
 local math_sin = math.sin
+local math_abs = math.abs
 local CurTime = CurTime
 
-local color1 = Color(14, 255, 151, 10)
-local color2 = Color(255, 255, 255, 40)
-local color3 = Color(255, 255, 255, 25)
-local color4 = Color(255, 255, 255, 7)
+local version_color = Color(255, 255, 255, 25)
 
 function PLUGIN:HUDPaint()
     if SETTINGS.options.Get("show_gamemode_info") then
         local a = ScrW() - W(10)
 
-        draw_SimpleText("yeah! arbitrage works :)", "arb.Font_FuturaPTBook_5", a, H(10), color1, TEXT_ALIGN_RIGHT)
+        local title_color = Color(255, 255, 255, 40 + math_abs(math_sin(CurTime() * 2) * 30))
 
-        draw_SimpleText("GM-Arbitrage Framework", "arb.Font_FuturaPTBook_7", a, H(35), color2, TEXT_ALIGN_RIGHT)
-        draw_SimpleText("v" .. Arbitrage.version, "arb.Font_FuturaPTBook_6", a, H(52), color3, TEXT_ALIGN_RIGHT)
+        draw_SimpleText("GM-Arbitrage Framework", "arb.Font_FuturaPTBook_6", a, H(5), title_color, TEXT_ALIGN_RIGHT)
+        draw_SimpleText("v" .. Arbitrage.version, "arb.Font_FuturaPTBook_5", a, H(23), version_color, TEXT_ALIGN_RIGHT)
 
-        draw_SimpleText("This gamemode is still in the early stages of development!\nIf you find any errors, please let us know.", "arb.Font_FuturaPTBook_5", a, H(80), color4, TEXT_ALIGN_RIGHT)
+        draw_SimpleText("AsterionLibrary", "arb.Font_FuturaPTBook_6", a, H(50), title_color, TEXT_ALIGN_RIGHT)
+        draw_SimpleText("v" .. asterionlib.version, "arb.Font_FuturaPTBook_5", a, H(68), version_color, TEXT_ALIGN_RIGHT)
 
-        local alpha = math_sin(CurTime() * 1) * 7
-        draw_SimpleText("❤ Made with love by Asterion", "arb.Font_FuturaPTBook_4", a, H(110), Color(255, 255, 255, alpha), TEXT_ALIGN_RIGHT)
+        local dev_color = Color(255, 255, 255, math_sin(CurTime()) * 20)
+        draw_SimpleText("❤ Made with love by Asterion", "arb.Font_FuturaPTBook_4", a, H(90), dev_color, TEXT_ALIGN_RIGHT)
     end
 end

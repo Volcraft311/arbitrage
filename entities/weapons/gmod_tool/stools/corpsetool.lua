a1de70fbe80a0192d2eb58a336edf2650c59bbb6
Selenter <samkolp@mail.ru>
2022-10-10 00:34:55 +0300

    Обновление для Corpse Tool

--- entities/weapons/gmod_tool/stools/corpsetool.lua
+++ entities/weapons/gmod_tool/stools/corpsetool.lua
@@ -7,6 +7,8 @@ TOOL.Information = {
     {name = "right", stage = 0}
 }
 
+TOOL.ClientConVar.steamid = ""
+
 if CLIENT then
     language.Add("tool.corpsetool.name", "Corpse Tool")
     language.Add("tool.corpsetool.desc", "Позволяет вам делать из Entity труп")
@@ -14,6 +16,7 @@ if CLIENT then
     language.Add("tool.corpsetool.right", "Нажмите правую кнопку мышки чтобы удалить из Entity труп")
 end
 
+local l = "corpsetool_"
 function TOOL:LeftClick()
     if CLIENT then return true end
 
@@ -23,7 +26,14 @@ function TOOL:LeftClick()
     if !IsValid(entity) then return client:ChatPrint("Не валидное Entity!") end
 
     if !entity:IsCorpse() then
-        entity:SetCorpse(true)
+        local convar = GetConVar(l .. "steamid"):GetString()
+
+        local steamid = true
+        if convar and convar != "" then
+            steamid = util.SteamIDFrom64(convar)
+        end
+
+        entity:SetCorpse(steamid)
         client:ChatPrint("Вы успешно сделали из " .. tostring(entity) .. " труп.")
     else
         client:ChatPrint(tostring(entity) .. " уже является трупом!")
@@ -39,9 +49,48 @@ function TOOL:RightClick()
     if !IsValid(entity) then return client:ChatPrint("Не валидное Entity!") end
 
     if entity:IsCorpse() then
-        entity:SetCorpse(false)
+        entity:SetCorpse(nil)
         client:ChatPrint("Вы успешно убрали из " .. tostring(entity) .. " труп.")
     else
         client:ChatPrint(tostring(entity) .. " не является трупом!")
     end
+end
+
+function TOOL.BuildCPanel(CPanel)
+    CPanel:AddControl("Header",{
+        Description = "Данный инструмент поможет вам превращать Entity в труп."
+    })
+
+    local PlayersDesc = vgui.Create("DLabel")
+    PlayersDesc:SetText("Выберите игрока который является убийцей:")
+    PlayersDesc:SetWrap(true)
+    PlayersDesc:SetAutoStretchVertical(true)
+    PlayersDesc:SetTextColor(Color(10,149,255))
+    CPanel:AddPanel(PlayersDesc)
+
+    local Combobox = vgui.Create("DComboBox")
+    Combobox.OnSelect = function(_, index, value, data)
+        RunConsoleCommand(l .. "steamid", data)
+    end
+
+    local function reload()
+        RunConsoleCommand(l .. "steamid", "")
+
+        Combobox:Clear()
+        Combobox:AddChoice("Не указан", "", true)
+
+        for k, v in ipairs(player.GetAll()) do
+            Combobox:AddChoice(v:FullName(true), v:SteamID64())
+        end
+    end
+    reload()
+
+    local UpdateButton = vgui.Create("DButton")
+    UpdateButton:SetText("Обновить список игроков")
+    UpdateButton.DoClick = function()
+        reload()
+    end
+
+    CPanel:AddPanel(UpdateButton)
+    CPanel:AddPanel(Combobox)
 end

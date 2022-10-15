6bd2fba9fa340b0e3ca962059895dea9a1644a76
Nitorogi <101595067+Nitorogi@users.noreply.github.com>
2022-10-15 10:57:02 +0300

    0.42

--- gamemode/framework/plugins/characters/sh_emoji_list.lua
+++ gamemode/framework/plugins/characters/sh_emoji_list.lua
@@ -1675,6 +1675,20 @@ Character.emoji:Register("michiru", {
     }
 })
 
+Character.emoji:Register("iris", {
+    ["Основные"] = {
+		"1.png",
+		"2.png",
+		"3.png",
+		"4.png",
+		"5.png",
+		"6.png",
+		"7.png",
+		"8.png",
+		"9.png",
+    }
+})
+
 
 
 

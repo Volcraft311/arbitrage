d642fffe0e4bd9a59c2c643a32dc9535870871d3
Selenter <samkolp@mail.ru>
2022-11-21 01:00:34 +0300

    Изменение позиции отрисовки менюшки инвентаря

--- gamemode/framework/plugins/inventorybase/derma/cl_menu.lua
+++ gamemode/framework/plugins/inventorybase/derma/cl_menu.lua
@@ -41,7 +41,7 @@ function PANEL:Init()
     inventoryPanel:SetInventory(self.inventory)
 
     local w, h, sizeH = inventoryPanel:GetWide(), inventoryPanel:GetTall(), H(90)
-    inventoryPanel:SetPos(ScrW() - w - W(278), ScrH() / 2 - h + sizeH / 2)
+    inventoryPanel:SetPos(ScrW() - w - W(278), ScrH() / 2 - h / 2)
     inventoryPanel.HoveredItem = function(_, item)
         if self.item == item then return end
 

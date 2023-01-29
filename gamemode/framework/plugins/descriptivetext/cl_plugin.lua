6fe6ea9f535777bac5293d0688c50a20cb0de164
Selenter <samkolp@mail.ru>
2023-01-29 19:32:48 +0300

    Дистанция для Descriptive Text

--- gamemode/framework/plugins/descriptivetext/cl_plugin.lua
+++ gamemode/framework/plugins/descriptivetext/cl_plugin.lua
@@ -37,7 +37,7 @@ function PLUGIN:HUDPaint()
 
 		local x, y = data2D.x, data2D.y
 		local distance = eyePos:Distance(pos)
-		local alpha = 255 - distance * 0.7
+		local alpha = 255 - distance
 
 		for k2, v2 in ipairs(data) do
 			draw.SimpleText(v2, font, x, y + fontHeight * k2 - (fontHeight * #data) / 2, Color(255, 255, 255, alpha), TEXT_ALIGN_CENTER)

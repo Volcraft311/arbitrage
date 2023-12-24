1e8f858cad9d942cc96f687686946c7212d44470
samkolp@mail.ru <samkolp@mail.ru>
2023-12-24 18:01:50 +1000

    update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -26,7 +26,7 @@ for k, v in pairs(gamemode_table) do
 end
 
 Arbitrage = Arbitrage or {}
-Arbitrage.version = "0.120 (05.12.2023)"
+Arbitrage.version = "0.121 (24.12.2023)"
 Arbitrage.GM = GM
 
 include("framework/sh_batch.lua")

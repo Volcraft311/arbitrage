e4bc8b9ec152e3841430faedbd70f561cea2d106
selenter-c <samkolp@mail.ru>
2024-05-20 09:55:39 +1000

    Обновление версии режима

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -26,7 +26,7 @@ for k, v in pairs(gamemode_table) do
 end
 
 Arbitrage = Arbitrage or {}
-Arbitrage.version = "0.128 (13.05.2024)"
+Arbitrage.version = "0.129 (20.05.2024)"
 Arbitrage.GM = GM
 
 include("framework/sh_batch.lua")

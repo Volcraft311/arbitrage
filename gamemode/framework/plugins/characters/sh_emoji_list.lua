f77c464f2f82f878bf2eee81851d7e8e6a515b31
Nitorogi <101595067+Nitorogi@users.noreply.github.com>
2024-07-20 04:46:47 +0300

    Альтернативная категория спрайтов у Монаки

--- gamemode/framework/plugins/characters/sh_emoji_list.lua
+++ gamemode/framework/plugins/characters/sh_emoji_list.lua
@@ -1588,7 +1588,34 @@ Character.emoji:Register("monaca", {
 		"14.png",
 		"15.png",
 		"16.png",
-		"17.png"
+		"17.png",
+		"18.png",
+		"19.png",
+		"20.png",
+		"21.png"
+	}
+	["На коляске"] = {
+		"22.png",
+		"23.png",
+		"24.png",
+		"25.png",
+		"26.png",
+		"27.png",
+		"28.png",
+		"29.png",
+		"30.png",
+		"31.png",
+		"32.png",
+		"33.png",
+		"34.png",
+		"35.png",
+		"36.png",
+		"37.png",
+		"38.png",
+		"39.png",
+		"40.png",
+		"41.png",
+		"42.png"
 	}
 })
 

764eeaa03f12eaa1a4bd0bfc9db0c4e7cd0a9b47
Selenter <samkolp@mail.ru>
2024-07-28 01:47:15 +0300

    Запрещаем игроку в регдулле разговаривать

--- gamemode/framework/plugins/voicedist/sv_plugin.lua
+++ gamemode/framework/plugins/voicedist/sv_plugin.lua
@@ -3,6 +3,7 @@ local PLUGIN = PLUGIN
 local dist = 650
 function PLUGIN:PlayerCanHearPlayersVoice(listener, talker)
     if talker:IsSpectate() then return false end
+    if talker:IsRagdolling() then return false end
     if !talker:oldAlive() then return false end
 
     if Arbitrage.lawEnable then
@@ -24,16 +25,16 @@ function PLUGIN:PlayerCanHearPlayersVoice(listener, talker)
         return true, false
     end
 
+    -- глобальный войс
     if talker:GetLocalVar("arbGlobalVoice") then
         return true, false
     end
 
-    local GetVoiceScale = talker:GetNetVar("arb.voicescale", 0.5)
-
+    local getVoiceScale = talker:GetNetVar("arb.voicescale", 0.5)
     if listener:IsSpectating() then
         -- там где находится камера
         local position = listener._CameraPosition
-        if position and position:Distance(talker:GetPos()) <= dist * GetVoiceScale then
+        if position and position:Distance(talker:GetPos()) <= dist * getVoiceScale then
             return true, false
         end
 
@@ -42,26 +43,25 @@ function PLUGIN:PlayerCanHearPlayersVoice(listener, talker)
         if IsValid(entity) then
             position = entity:IsPlayer() and entity:GetShootPos() or entity:GetPos()
 
-            if position and position:Distance(talker:GetPos()) <= dist * GetVoiceScale then
+            if position and position:Distance(talker:GetPos()) <= dist * getVoiceScale then
                 return true, false
             end
         end
 
         -- для тех кто рядом со мной (обычная позиция без спектейта)
-        if listener:GetPos():Distance(talker:GetPos()) <= dist * GetVoiceScale then
+        if listener:GetPos():Distance(talker:GetPos()) <= dist * getVoiceScale then
             return true, false
         end
     end
 
-    if listener:GetPos():Distance(talker:GetPos()) > dist * GetVoiceScale then return false end
+    if listener:GetPos():Distance(talker:GetPos()) > dist * getVoiceScale then return false end
 
     return true, true
 end
 
 netstream.Hook("VOICEDIST:ChangeVoiceVolume", function(client, data)
-    local GetVoiceScale = client:GetNetVar("arb.voicescale", 0.5)
-
-    local amount = GetVoiceScale + (data and 0.1 or -0.1)
+    local getVoiceScale = client:GetNetVar("arb.voicescale", 0.5)
+    local amount = getVoiceScale + (data and 0.1 or -0.1)
     amount = math.Clamp(amount, 0.1, 1)
 
     client:SetNetVar("arb.voicescale", amount)

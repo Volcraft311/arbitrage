6ac4dfcbcafeacf015b2b1a894db502ba97f8fdc
Selenter <selenter.c@gmail.com>
2024-11-12 14:03:16 +0300

    Метод конвертации Кириллицы в Латиницу

--- gamemode/framework/system/sh_commands.lua
+++ gamemode/framework/system/sh_commands.lua
@@ -30,11 +30,59 @@ Arbitrage.commands.types = {
     end,
 }
 
+Arbitrage.commands.keyList = {
+    й = "q",
+    ц = "w",
+    у = "e",
+    к = "r",
+    е = "t",
+    н = "y",
+    г = "u",
+    ш = "i",
+    щ = "o",
+    з = "p",
+    х = "[",
+    ъ = "]",
+    ф = "a",
+    ы = "s",
+    в = "d",
+    а = "f",
+    п = "g",
+    р = "h",
+    о = "j",
+    л = "k",
+    д = "l",
+    ж = ";",
+    э = "'",
+    я = "z",
+    ч = "x",
+    с = "c",
+    м = "v",
+    и = "b",
+    т = "n",
+    ь = "m",
+    б = ",",
+    ю = ".",
+    ["."] = "/"
+}
+
 Arbitrage.commands.fault = {
     [1] = "НЕИЗВЕСТНО",
     [2] = "Вы указали ошибку в аргументе",
     [3] = "У вас не указан аргумент"
 }
+
+function Arbitrage.commands.ConvertRusToEng(data)
+    local newData = ""
+    for i = 1, data:utf8len() do
+        local s = data:utf8sub(i, i):utf8lower()
+
+        local n_s = Arbitrage.commands.keyList[s] or s
+        newData = newData .. n_s
+    end
+
+    return newData
+end
 
 function Arbitrage.commands.WordsInArray(data)
     if !data then return end

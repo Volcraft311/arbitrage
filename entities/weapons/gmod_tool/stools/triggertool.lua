bcb140c101962f7e94ce641590d876da3b708388
volcraft <Vladimirskeletons@gmail.com>
2024-12-16 00:41:22 +0200

    Обновление triggertool.lua

--- entities/weapons/gmod_tool/stools/triggertool.lua
+++ entities/weapons/gmod_tool/stools/triggertool.lua
@@ -60,8 +60,8 @@ function TOOL:LeftClick(trace)
         Trigger:GetByID(id):SetPoint(point, vector)
         netstream.Start("Trigger:SetPos",{id = id, point = point, vector = vector})
         Trigger.UpdateToolPanel()
+        Trigger.drawTriggers = true
     end
-    Trigger.drawTriggers = true
 end
 
 -- Правая кнопка мыши
@@ -74,8 +74,8 @@ function TOOL:RightClick(trace)
         Trigger:GetByID(id):SetPoint(point, vector)
         netstream.Start("Trigger:SetPos",{id = id, point = point, vector = vector})
         Trigger.UpdateToolPanel()
+        Trigger.drawTriggers = true
     end
-    Trigger.drawTriggers = true
 end
 
 -- Перезарядка
@@ -84,13 +84,14 @@ function TOOL:Reload(trace)
     if SERVER then
         Trigger:Create({points = {trace.HitPos,trace.HitPos + Vector(5,5,5)},type = 1})
         Trigger:SyncLast()
+        Arbitrage.adminnotify:SendNotify("triggercreated", self:GetOwner():FullName(), Trigger:GetLast().name)
     else
         timer.Simple(0.1,function()
             Trigger.UpdateToolPanel()
             Trigger.selectedID = #Trigger.instances
         end)
+        Trigger.drawTriggers = true
     end
-    Trigger.drawTriggers = true
     return true
 end
 
@@ -101,7 +102,7 @@ function TOOL:DrawHUD()
         local vector = (v.points[1] + v.points[2]) / 2
         local vScreen = vector:ToScreen()
         draw.RoundedBox(1,vScreen.x - 20,vScreen.y - 20,40,40,color_text_background)
-        draw.SimpleText(k,"Trebuchet24",vScreen.x,vScreen.y,color_text,TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
+        draw.SimpleText(v.name,"Trebuchet24",vScreen.x,vScreen.y,color_text,TEXT_ALIGN_CENTER,TEXT_ALIGN_CENTER)
     end
 
     -- Точки триггера
@@ -130,11 +131,129 @@ function TOOL.BuildCPanel(CPanel)
     TriggerList:SetTall(500)
 
 
-    TriggerList.OnRowSelected = function(panel, index, row)
-        Trigger.selectedID = tonumber(row:GetValue(2))
-    end
 
     Trigger.TriggerList = TriggerList
 
     Trigger.UpdateToolPanel()
+
+
+    local ActionList = vgui.Create("DListView", CPanel)
+    ActionList:Dock(TOP)
+    ActionList:DockMargin(10, 20, 10, 40)
+    ActionList:AddColumn( "ActionID" )
+    ActionList:AddColumn( "Action" )
+    ActionList:AddColumn( "Args" )
+    ActionList:SetTall(500)
+    Trigger.ActionList = ActionList
+
+
+
+    TriggerList.OnRowSelected = function(panel, index, row)
+        Trigger.selectedID = tonumber(row:GetValue(2))
+        Trigger.UpdateActionList()
+    end
+
+    TriggerList.OnRowRightClick = function(panel, index, row)
+        local Menu = DermaMenu()
+        local thisTrigger = Trigger:GetSelected()
+        Menu:AddOption( "Добавить пустое действие на вход", function()
+            netstream.Start("Trigger:AddEnterAction",{id = Trigger.selectedID,actionid = 1, args = {"say","/me test message"}})
+            timer.Simple(0.1,function()
+                Trigger.UpdateActionList()
+            end)
+        end)
+        Menu:AddSpacer()
+
+        Menu:AddOption( "Изменить имя", function()
+            local str_arg = vgui.Create("DFrame")
+            str_arg:SetSize(500, 100)
+            str_arg:Center()
+            str_arg:MakePopup()
+            str_arg:SetTitle("Указать имя триггера")
+
+            local str_button = vgui.Create("DButton",str_arg)
+            str_button:SetText("Применить изменения")
+            str_button:Dock(BOTTOM)
+            str_button:SetWidth(20)
+
+
+            local label = str_arg:Add("DTextEntry")
+            label:Dock(TOP)
+            label:SetText(thisTrigger.name)
+
+            str_button.DoClick = function()
+                thisTrigger.name = label:GetText()
+                print(thisTrigger.name)
+                netstream.Start("Trigger:ChangeName",{id = thisTrigger.id,name = thisTrigger.name})
+                str_arg:Remove()
+            end
+            timer.Simple(0.1,function()
+                Trigger.UpdateActionList()
+            end)
+        end)
+
+        Menu:AddOption( "Удалить триггер", function()
+            netstream.Start("Trigger:Remove",{id = Trigger.selectedID})
+            timer.Simple(0.1,function()
+                Trigger.UpdateActionList()
+            end)
+        end)
+
+        Menu:Open()
+    end
+
+    ActionList.OnRowRightClick = function(panel, index, row)
+        local Menu = DermaMenu()
+        Menu:AddOption( "Удалить Действие", function()
+            netstream.Start("Trigger:RemoveEnterAction",{id = Trigger.selectedID,number = index})
+            timer.Simple(0.1,function()
+                Trigger.UpdateActionList()
+            end)
+        end)
+
+
+        Menu:AddSpacer()
+
+        local SubMenu = Menu:AddSubMenu( "Изменить Аргументы" )
+        local thisAction = Trigger:ActionByID(tonumber(row:GetValue(1)))
+        local thisTrigger = Trigger:GetSelected()
+        for k, v in pairs(thisAction.args) do
+            local arg = SubMenu:AddOption(thisAction.args_desc[k], function()
+                local str_arg = vgui.Create("DFrame")
+                str_arg:SetSize(500, 100)
+                str_arg:Center()
+                str_arg:MakePopup()
+                str_arg:SetTitle("Указать Аргумент " .. k)
+
+                local str_button = vgui.Create("DButton",str_arg)
+                str_button:SetText("Применить изменения")
+                str_button:Dock(BOTTOM)
+                str_button:SetWidth(20)
+
+                if v == "string" then
+                    local label = str_arg:Add("DTextEntry")
+                    label:Dock(TOP)
+                    label:SetText(thisTrigger.EnterActionList[index].args[k])
+
+                    str_button.DoClick = function()
+                        thisTrigger.EnterActionList[index].args[k] = label:GetText()
+                        netstream.Start("Trigger:EditEnterAction",{id = Trigger.selectedID,number = index,args = thisTrigger.EnterActionList[index].args})
+                        str_arg:Remove()
+                    end
+                elseif v == "number" then
+                    local label = str_arg:Add("DTextEntry")
+                    label:Dock(TOP)
+                    label:SetText(tostring(thisTrigger.EnterActionList[index].args[k]))
+
+                    str_button.DoClick = function()
+                        thisTrigger.EnterActionList[index].args[k] = tonumber(label:GetText())
+                        netstream.Start("Trigger:EditEnterAction",{id = Trigger.selectedID,number = index,args = thisTrigger.EnterActionList[index].args})
+                        str_arg:Remove()
+                    end
+                end
+            end)
+            arg:SetIcon( "icon16/group.png" )
+        end
+        Menu:Open()
+    end
 end
--- gamemode/framework/plugins/triggers/sh_trigger_types.lua
+++ gamemode/framework/plugins/triggers/sh_trigger_types.lua
@@ -1,53 +1,15 @@
--- Trigger:CreateType("TakeDamage",{
---     name = "Получение урона",
---     settings = {
---         {
---             id = "DamageCount",
---             name = "Получение урона",
---             type = "slider"
---         },
---         {
---             id = "Delay",
---             name = "Задержка",
---             type = "slider"
---         }
---     },
---     onEnter = function(client)
---     end,
-
---     onCanInteract = function(client)
---         return client:Alive()
---     end,
-
---     onExit = function(client)
---     end
--- })
-
-
-Trigger:CreateType({
-    name = "Тест",
-    OnEnter = function(client)
-        if SERVER then
-            client:FallOver(2)
-            Arbitrage.commands.PlayerSay(client, "/me упала с обрыва")
-            timer.Simple(1,function()
-                timer.Create("TriggerTest:IsOnGround",0.2,400,function()
-                    if client:IsOnGround() then
-                        Arbitrage.commands.PlayerSay(client, "/me сломала свою ногу")
-                        client:AddTemporaryStatusEffect("broken_leg",20)
-                        timer.Remove("TriggerTest:IsOnGround")
-                    end
-                end)
+Trigger.actionList = {
+    {
+        name = "Написать в консоль",
+        run = function(trigger, args)
+            local delay = args[3]
+            timer.Simple(delay or 0,function()
+                if CLIENT then
+                    RunConsoleCommand(args[1],args[2])
+                end
             end)
-        end
-    end
-})
-
-Trigger:CreateType({
-    name = "Adolfus",
-    OnEnter = function(client)
-        if CLIENT then
-            print("Acthung")
-        end
-    end
-})
+        end,
+        args = {"string","string","number"},
+        args_desc = {"Консольная Команда", "Аргументы команды", "Задержка до выполнения"}
+    }
+}

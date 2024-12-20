6901248a19fd1296dd35e02f958e485d7a113676
Selenter <selenter.c@gmail.com>
2024-12-20 08:51:32 +0300

    Оптимизация системы Firstperson

--- gamemode/framework/plugins/firstperson/cl_hooks.lua
+++ gamemode/framework/plugins/firstperson/cl_hooks.lua
@@ -16,6 +16,11 @@ local PLUGIN = PLUGIN
 PLUGIN.isAllow = false
 
 -- Localize Global Calls
+local PLAYER = FindMetaTable("Player")
+local ENTITY = FindMetaTable("Entity")
+local VECTOR = FindMetaTable("Vector")
+local CUSERCMD = FindMetaTable("CUserCmd")
+
 local IsValid = IsValid
 local RealFrameTime = RealFrameTime
 local LerpAngle = LerpAngle
@@ -26,9 +31,44 @@ local math_Clamp = math.Clamp
 local Vector = Vector
 local util_TraceLine = util.TraceLine
 local select = select
-local FrameTime = FrameTime
 local concommand_Add = concommand.Add
-local EyeAngles = EyeAngles
+local timer_Simple = timer.Simple
+
+local IsNocliping = PLAYER.IsNocliping
+local oldAlive = PLAYER.oldAlive
+local IsPlaying = PLAYER.IsPlaying
+local IsPlayingTaunt = PLAYER.IsPlayingTaunt
+local IsSpectating = PLAYER.IsSpectating
+local GetActiveWeapon = PLAYER.GetActiveWeapon
+local GetAction = PLAYER.GetAction
+local InVehicle = PLAYER.InVehicle
+local Team = PLAYER.Team
+local GetAimVector = PLAYER.GetAimVector
+local KeyDown = PLAYER.KeyDown
+local HasTemporaryStatusEffect = PLAYER.HasTemporaryStatusEffect
+local SetEyeAngles = PLAYER.SetEyeAngles
+
+local GetNetVar = ENTITY.GetNetVar
+local GetClass = ENTITY.GetClass
+local GetAttachment = ENTITY.GetAttachment
+local LookupAttachment = ENTITY.LookupAttachment
+local EyeAngles = ENTITY.EyeAngles
+local GetVelocity = ENTITY.GetVelocity
+local GetRight = ENTITY.GetRight
+
+local Length2D = VECTOR.Length2D
+
+local GetViewAngles = CUSERCMD.GetViewAngles
+local SetViewAngles = CUSERCMD.SetViewAngles
+timer_Simple(0, function() -- overwrite gamemodes...
+	IsNocliping = PLAYER.IsNocliping
+	oldAlive = PLAYER.oldAlive
+	IsPlaying = PLAYER.IsPlaying
+	IsSpectating = PLAYER.IsSpectating
+	GetAction = PLAYER.GetAction
+	HasTemporaryStatusEffect = PLAYER.HasTemporaryStatusEffect
+end)
+
 
 PLUGIN.name = "First Person"
 
@@ -59,26 +99,26 @@ local function allow()
 
 	if Arbitrage.IsThirdPerson() then return false end
 	if Arbitrage.lawEnable then return false end
-	if client:IsNocliping() then return false end
-	if client:GetNetVar("inbed") then return false end
+	if IsNocliping(client) then return false end
+	if GetNetVar(client, "inbed") then return false end
 
 	if !IsValid(client) then return true end
-	if !client:oldAlive() then return false end
-	if !client:IsPlaying() then return false end
-	if client:IsPlayingTaunt() then return false end
-	if client:IsSpectating() then return false end
+	if !oldAlive(client) then return false end
+	if !IsPlaying(client) then return false end
+	if IsPlayingTaunt(client) then return false end
+	if IsSpectating(client) then return false end
 
-	local weapon = client:GetActiveWeapon()
+	local weapon = GetActiveWeapon(client)
 	if !IsValid(weapon) then return true end
 
-	local class = weapon:GetClass()
+	local class = GetClass(weapon)
 	if !class then return true end
 
 	if class == "academy_first" and weapon:GetAttack() then
 		return false
 	end
 
-	local bThirdPerson = select(3, client:GetAction())
+	local bThirdPerson = select(3, GetAction(client))
 	if bThirdPerson then return false end
 
 	if d_weapon[class] then return false end
@@ -89,7 +129,7 @@ end
 function PLUGIN:ShouldDrawLocalPlayer()
 	if !self.isAllow then return end
 
-	if traceHit and !LocalPlayer():InVehicle() then
+	if traceHit and !InVehicle(LocalPlayer()) then
 		return false
 	else
 		return true
@@ -103,7 +143,7 @@ function PLUGIN:CalcView(client, pos, angles, fov)
 	if !self.isAllow then return end
 
 	local cameraPos = nil
-	local character = Character.team.instances[client:Team()]
+	local character = Character.team.instances[Team(client)]
 	if character then
 		local characterCameraPos = character.cameraPos
 		if characterCameraPos then
@@ -111,12 +151,12 @@ function PLUGIN:CalcView(client, pos, angles, fov)
 		end
 	end
 
-	eyeAtt = client:GetAttachment(client:LookupAttachment("eyes"))
-	local forwardVec = client:GetAimVector()
+	eyeAtt = GetAttachment(client, LookupAttachment(client, "eyes"))
+	local forwardVec = GetAimVector(client)
 	local FT = RealFrameTime()
-	local eyeAngles = client:EyeAngles()
+	local eyeAngles = EyeAngles(client)
 
-	if (traceHit and !client:InVehicle()) or !eyeAtt then
+	if (traceHit and !InVehicle(client)) or !eyeAtt then
 		return
 	end
 
@@ -135,34 +175,29 @@ function PLUGIN:CalcView(client, pos, angles, fov)
 	ViewOffsetLeftRight = math_Approach(ViewOffsetLeftRight, 0, 0.5)
 
 	local view = {}
-	if client:WaterLevel() >= 3 then
-		ViewOffsetUp = math_Approach(ViewOffsetUp, 0, 0.5)
-		ViewOffsetForward = math_Approach(ViewOffsetForward, 8, 0.5)
-		RollDependency = Lerp(FT * 15, RollDependency, 0.5)
-	else
-		ViewOffsetUp = math_Approach(ViewOffsetUp, math_Clamp(eyeAngles.p * -0.1, 0, 10), 0.5)
-		ViewOffsetForward = math_Approach(ViewOffsetForward, 5 + math_Clamp(eyeAngles.p * 0.1, 0, 5), 0.5)
-		RollDependency = Lerp(FT * 15, RollDependency, 0.05)
-	end
+
+	ViewOffsetUp = math_Approach(ViewOffsetUp, math_Clamp(eyeAngles.p * -0.1, 0, 10), 0.5)
+	ViewOffsetForward = math_Approach(ViewOffsetForward, 5 + math_Clamp(eyeAngles.p * 0.1, 0, 5), 0.5)
+	RollDependency = Lerp(FT * 15, RollDependency, 0.05)
 
 	if eyeAtt then
-		view.origin = eyeAtt.Pos + (Vector(forwardVec.x * (ViewOffsetForward + ViewOffsetForward2), forwardVec.y * (ViewOffsetForward + ViewOffsetForward2 - 0.3), 0)) + Vector(0, 0, ViewOffsetUp) + client:GetRight() * ViewOffsetLeftRight
+		view.origin = eyeAtt.Pos + (Vector(forwardVec.x * (ViewOffsetForward + ViewOffsetForward2), forwardVec.y * (ViewOffsetForward + ViewOffsetForward2 - 0.3), 0)) + Vector(0, 0, ViewOffsetUp) + GetRight(client) * ViewOffsetLeftRight
 		view.angles = CurView
 
-		local shift = client:GetVelocity():Length2D() * 0.02
+		local shift = Length2D(GetVelocity(client)) * 0.02
 		local value = 0
-		if client:KeyDown(IN_FORWARD) then
+		if KeyDown(client, IN_FORWARD) then
 			value = shift
-		elseif client:KeyDown(IN_BACK) then
+		elseif KeyDown(client, IN_BACK) then
 			value = -shift
 		end
 
-		if client:HasTemporaryStatusEffect("berserk") then
+		if HasTemporaryStatusEffect(client, "berserk") then
 			value = value + 15
 		end
 
 		value = math_Clamp(value, -8, 8)
-		fovShift = Lerp(FrameTime() * 3, fovShift, value)
+		fovShift = Lerp(FT * 3, fovShift, value)
 
 		if cameraPos then
 			view.origin = view.origin + cameraPos
@@ -170,7 +205,7 @@ function PLUGIN:CalcView(client, pos, angles, fov)
 
 		view.fov = fov + fovShift
 
-		return GAMEMODE:CalcView(client, view.origin, view.angles, view.fov, view.znear)
+		return view
 	end
 end
 
@@ -180,7 +215,7 @@ function PLUGIN:Think()
 
 	local client = LocalPlayer()
 	if eyeAtt then
-		local forwardVec = client:GetAimVector()
+		local forwardVec = GetAimVector(client)
 
 		local tr = {}
 		tr.start = eyeAtt.Pos
@@ -203,13 +238,13 @@ function PLUGIN:CreateMove(ucmd)
 	local m = 75 -- LocalPlayer():Team() == TEAM_HIFUMI and 55 or 75
 	local s = 90 -- LocalPlayer():Team() == TEAM_MONDO and 32 or 90
 
-	local eyeAng = ucmd:GetViewAngles()
-	ucmd:SetViewAngles(Angle(math_Clamp(eyeAng.p, -s, m), eyeAng.y, eyeAng.r))
+	local eyeAng = GetViewAngles(ucmd)
+	SetViewAngles(ucmd, Angle(math_Clamp(eyeAng.p, -s, m), eyeAng.y, eyeAng.r))
 end
 
 
 concommand_Add("arb_camerafix", function(client, cmd, args)
 	local ang = EyeAngles()
 
-	client:SetEyeAngles(Angle(ang.p, ang.y, 0))
+	SetEyeAngles(client, Angle(ang.p, ang.y, 0))
 end)

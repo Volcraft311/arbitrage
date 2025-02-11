b3655674377ac8019610cf4b95b1ddc0f88e72a8
Selenter <selenter.c@gmail.com>
2025-02-11 18:12:26 +0300

    Добавление файла russian.lua

---
+++ gamemode/framework/languages/sh_russian.lua
@@ -0,0 +1,58 @@
+--[[
+        © AsterionStaff 2025.
+        This script was created from the developers of the Asterion Staff.
+        You can get more information from one of the links below:
+            Site - https://asterion.games
+            Discord - https://discord.gg/Np5evb5ZsR
+        
+        developer(s):
+            Selenter - https://steamcommunity.com/id/selenter
+
+        ——— Chop your own wood and it will warm you twice.
+]]--
+
+
+Arbitrage.language:Add("ru", {
+    name = "Русский"
+}, {
+    ["#chat_say"] = "говорит",
+    ["#chat_whispers"] = "шепчет",
+    ["#chat_yell"] = "кричит",
+    ["#chat_anonymously"] = "Анонимно",
+
+    ["#chat_lnrp_type"] = "Локальный НонРП чат",
+    ["#chat_gnrp_type"] = "Локальный НонРП чат",
+    ["#chat_pm_type"] = "Личное сообщение",
+    ["#chat_admin_type"] = "Чат администрации",
+    ["#chat_help_type"] = "Помощь",
+    ["#chat_notification_type"] = "Уведомление",
+
+    ["#chat_nrp_chat_block"] = "Администрация запретила вам писать в NonRP чат!",
+
+    ["#try_successfully"] = "Удачно",
+    ["#try_unsuccessful"] = "Неудачно",
+
+    ["#emoji_smiling"] = "улыбается",
+    ["#emoji_sad"] = "грустит",
+    ["#emoji_rejoices"] = "радуется",
+    ["#emoji_shows_tongue"] = "показывает язык",
+    ["#emoji_surprised"] = "удивлен(а)",
+    ["#emoji_doubts"] = "сомневается",
+    ["#emoji_crying"] = "плачет",
+    ["#emoji_kisses"] = "целует",
+    ["#emoji_without_emotion"] = "без эмоций",
+    ["#emoji_confused"] = "смущен(а)",
+    ["#emoji_embarrassed"] = "засмущался(ась)",
+    ["#emoji_pursed_his_lips"] = "сжал(а) губы",
+    ["#emoji_smiles_slyly"] = "хитро улыбается",
+    ["#emoji_disappointed"] = "разочарован(а)",
+    ["#emoji_shocked"] = "в шоке",
+    ["#emoji_is_perplexed"] = "недоумевает",
+    ["#emoji_tired"] = "уставший(ая)",
+    ["#emoji_very_happy"] = "очень счастлив(а)",
+    ["#emoji_glad"] = "радостный(ая)",
+    ["#emoji_upset"] = "расстроенный(ая)",
+    ["#emoji_expresses_dissatisfaction"] = "выражает недовольство",
+    ["#emoji_expresses_joy"] = "выражает радость",
+    
+})

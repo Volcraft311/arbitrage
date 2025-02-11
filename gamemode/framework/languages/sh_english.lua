4d987e1dd9933fa1dd45c732f21f73a78d9144d4
Selenter <selenter.c@gmail.com>
2025-02-11 18:12:33 +0300

    Добавление файла english.lua

---
+++ gamemode/framework/languages/sh_english.lua
@@ -0,0 +1,22 @@
+--[[
+        © AsterionStaff 2025.
+        This script was created from the developers of the Asterion Staff.
+        You can get more information from one of the links below:
+            Site - https://asterion.games
+            Discord - https://discord.gg/Np5evb5ZsR
+        
+        developer(s):
+            Selenter - https://steamcommunity.com/id/selenter
+
+        ——— Chop your own wood and it will warm you twice.
+]]--
+
+
+Arbitrage.language:Add("en", {
+    name = "English"
+}, {
+    ["#test_1"] = "Testing 1",
+    ["#test_2"] = function() return "Testing 2" end,
+    ["#test_3"] = {"Testing", 3},
+    ["#test_4"] = "Testing argument %s"
+})

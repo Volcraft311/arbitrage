89a119b914c821051ec0f7b9427aa87d5c159952
Vladimirs <vladimirskeletons@gmail.com>
2026-05-28 19:32:19 +0300

    aa

--- gamemode/framework/plugins/lawsystem/cl_plugin.lua
+++ gamemode/framework/plugins/lawsystem/cl_plugin.lua
@@ -295,7 +295,7 @@ local function draw_3d_sprites(data)
             surface.DrawTexturedRect(-spriteShift, -spriteH/2, spriteW, spriteH )
             surface.SetDrawColor(255, 255, 255, 255)
             -- surface.DrawTexturedRect(-spriteShift, -spriteH/2, spriteW, spriteH)
-            surface.DrawTexturedRectUV(0, 0, spriteW, spriteH, 1, 0, 0, 1)
+            surface.DrawTexturedRectUV(-spriteShift, -spriteH/2, spriteW, spriteH, 1, 0, 0, 1)
         cam.End3D2D()
     end
 end

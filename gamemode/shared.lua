248ab521f6768149b244c3c92924b336c9dcb170
Vladimirs <vladimirskeletons@gmail.com>
2026-05-29 20:05:47 +0300

    Version 0.21

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -24,7 +24,7 @@ for _, v in ipairs(gamemode_table) do
 end
 
 Arbitrage = Arbitrage or {}
-Arbitrage.version = "0.2 (26.05.2026)"
+Arbitrage.version = "0.21 (29.05.2026)"
 Arbitrage.GM = GM
 
 include("dev.lua")
